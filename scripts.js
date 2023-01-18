12047c205d1deab1187d590b6e02dd6baa9338da
Justin Lee <justinlee2364@gmail.com>
2023-01-18 11:25:38 -0800

    fixed files

--- scripts.js
+++ scripts.js
@@ -1,21 +1,26 @@
 function toggleMenu() {
-    const menuToggle = document.querySelector('.toggle');
-    const sidebar = document.querySelector('.sidebar');
-    menuToggle.classList.toggle('active')
-    sidebar.classList.toggle('active')
+  const menuToggle = document.querySelector('.toggle');
+  const sidebar = document.querySelector('.sidebar');
+  menuToggle.classList.toggle('active')
+  sidebar.classList.toggle('active')
 }
 
 window.onload = function() {
-    let timeout;
+let timeout;
+const text = document.getElementById("cursorDesign");
+document.getElementById("bodyId").onscroll = function() {
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+    document.getElementById("langIcon").style.opacity = 1;
+    document.getElementById("toggle").style.opacity = 1;
+  }, 1000);
+  document.getElementById("langIcon").style.opacity = 0.3;
+  document.getElementById("toggle").style.opacity = 0.3;
+};
 
-    document.getElementById("bodyId").onscroll = function() {
-        clearTimeout(timeout);
-        timeout = setTimeout(function() {
-            document.getElementById("langIcon").style.opacity = 1;
-            document.getElementById("toggle").style.opacity = 1;
-        }, 1000);
-      document.getElementById("langIcon").style.opacity = 0.3;
-      document.getElementById("toggle").style.opacity = 0.3;
-    };
-  }
-  
+document.addEventListener("mousemove", function(e) {
+  text.style.left = (e.pageX - 15) + 'px';
+  text.style.top = (e.pageY - 25) + 'px';
+});
+}
+
